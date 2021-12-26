d1054e5df33bf0d9599bcbfd1dad51f76194d588
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2021-12-26 18:40:47 +0100

    ui: Draft changing one's display name

--- lib/ui/pages/profile.dart
+++ lib/ui/pages/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moxxyv2/ui/widgets/topbar.dart';
 import 'package:moxxyv2/ui/widgets/sharedmedia.dart';
 import 'package:moxxyv2/ui/widgets/avatar.dart';
+import 'package:moxxyv2/ui/constants.dart';
 import 'package:moxxyv2/models/conversation.dart';
 
 // TODO: Move to separate file
@@ -15,6 +16,21 @@ class ProfilePageArguments {
 }
 
 class SelfProfileHeader extends StatelessWidget {
+  // This is to keep the snackbar only on this page. This also removes it once
+  // we navigate away from this page.
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  // TODO
+  TextEditingController controller = TextEditingController(text: "Testuser");
+  bool _showingSnackBar = false;
+
+  SelfProfileHeader({ required this.scaffoldKey });
+  
+  void _applyDisplayNameChange() {
+    // TODO
+    // TODO: Maybe show a LinearProgressIndicator
+    this._showingSnackBar = false;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -23,16 +39,54 @@ class SelfProfileHeader extends StatelessWidget {
           radius: 110.0,
           avatarUrl: "https://3.bp.blogspot.com/-tXOVVeovbNA/XI8EEkbKjgI/AAAAAAAAJrs/3lOV4RQx9kIp9jWBmZhSKyng9iNQrDivgCLcBGAs/s2560/hatsune-miku-4k-fx-2048x2048.jpg",
           alt: Text("?"),
-          showEditButton: true,
+          showEditButton: false,
           onTapFunction: () {}
         ),
         Padding(
           padding: EdgeInsets.only(top: 8.0),
-          child: Text(
-            // TODO
-            "Testuser",
-            style: TextStyle(
-              fontSize: 30
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: 220
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                width: 1,
+                color: BUBBLE_COLOR_SENT
+              )
+            ),
+            child: TextField(
+              maxLines: 1,
+              controller: this.controller,
+              onChanged: (value) {
+                if (!this._showingSnackBar) {
+                  this._showingSnackBar = true;
+
+                  this.scaffoldKey.currentState!.showSnackBar(SnackBar(
+                      // TODO: The colors are kinda bad
+                      // TODO: This feels like one big hack
+                      duration: Duration(days: 1),
+                      backgroundColor: PRIMARY_COLOR,
+                      content: Text(
+                        "Display name not applied",
+                        style: TextStyle(
+                          color: Colors.white
+                        )
+                      ),
+                      action: SnackBarAction(
+                        label: "Apply",
+                        onPressed: this._applyDisplayNameChange,
+                        textColor: Colors.blue
+                      )
+                  ));
+                }
+              },
+              decoration: InputDecoration(
+                labelText: "Display name",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.0),
+                isDense: true
+              )
             )
           )
         ),
@@ -89,11 +143,14 @@ class ProfileHeader extends StatelessWidget {
 }
 
 class ProfilePage extends StatelessWidget {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context)!.settings.arguments as ProfilePageArguments;
     
     return Scaffold(
+      key: this.scaffoldKey,
       body: SafeArea(
         child: Stack(
           alignment: Alignment.center,
@@ -101,7 +158,7 @@ class ProfilePage extends StatelessWidget {
             Positioned(
               child: Column(
                 children: [
-                  args.isSelfProfile ? SelfProfileHeader() : ProfileHeader(conversation: args.conversation!),
+                  args.isSelfProfile ? SelfProfileHeader(scaffoldKey: this.scaffoldKey) : ProfileHeader(conversation: args.conversation!),
                   Visibility(
                     visible: !args.isSelfProfile && args.conversation!.sharedMediaPaths.length > 0,
                     child: args.isSelfProfile ? SizedBox() : SharedMediaDisplay(
