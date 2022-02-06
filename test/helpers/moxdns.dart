8bce32dcaf75eb54494d34eeea953d307c9923dc
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-02-06 19:05:12 +0100

    test: The list must be 'mutable' as we sort it

--- test/helpers/moxdns.dart
+++ test/helpers/moxdns.dart
@@ -3,8 +3,8 @@ import "dart:async";
 import "package:moxdns/moxdns.dart";
 
 Future<List<SrvRecord>> srvQueryStub(String domain, bool dnssec) async {
-  return const [
-    SrvRecord(
+  return [
+    const SrvRecord(
       target: "some.server",
       port: 5223,
       priority: 0,
