8e3ceaf52be2e4306d20924b1651fc3193c51dd5
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-02-28 20:58:49 +0100

    ui: Deal with files that are not found

---
+++ lib/ui/widgets/chat/filenotfound.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+
+class FileNotFound extends StatelessWidget {
+  const FileNotFound({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // TODO: Maybe make this prettier
+      decoration: const BoxDecoration(
+        color: Colors.black45,
+        borderRadius: BorderRadius.all(Radius.circular(18.0))
+      ),
+      child: Text(
+        "File not available",
+        style: TextStyle(
+          color: Colors.red[600]
+        )
+      )
+    );
+  }
+}
