3c51a19636b640b08f79eb6c7e8c2379359773b5
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-04-17 23:36:15 +0200

    xmpp: Remove _buffer from XmlStreamBuffer

--- lib/xmpp/buffer.dart
+++ lib/xmpp/buffer.dart
@@ -6,14 +6,11 @@ import "package:xml/xml.dart";
 import "package:xml/xml_events.dart";
 
 class XmlStreamBuffer extends StreamTransformerBase<String, XMLNode> {
-  String _buffer;
   final StreamController<XMLNode> _streamController;
   final XmlNodeDecoder _decoder;
 
   XmlStreamBuffer()
-    : _buffer = "",
-      _streamController = StreamController(),
-      _decoder = XmlNodeDecoder();
+    : _streamController = StreamController(), _decoder = XmlNodeDecoder();
 
   @override
   Stream<XMLNode> bind(Stream<String> stream) {
@@ -26,8 +23,6 @@ class XmlStreamBuffer extends StreamTransformerBase<String, XMLNode> {
 
         return true;
     }).transform(_decoder).listen((nodes) {
-
-        print(nodes);
         for (final node in nodes) {
           if (node.nodeType == XmlNodeType.ELEMENT) {
             _streamController.add(XMLNode.fromXmlElement(node as XmlElement));
