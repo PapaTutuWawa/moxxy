04073eba36f1f471b24ba88b3c84d441060b6e0c
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-07-16 21:43:25 +0200

    test: Fix reconnection test

--- lib/service/connectivity.dart
+++ lib/service/connectivity.dart
@@ -5,6 +5,7 @@ import "package:moxxyv2/service/moxxmpp/reconnect.dart";
 import "package:logging/logging.dart";
 import "package:connectivity_plus/connectivity_plus.dart";
 import "package:get_it/get_it.dart";
+import "package:meta/meta.dart";
 
 class ConnectivityService {
   final Logger _log;
@@ -14,6 +15,12 @@ class ConnectivityService {
 
   ConnectivityService() : _log = Logger("ConnectivityService");
 
+  @visibleForTesting
+  void setConnectivity(ConnectivityResult result) {
+    _log.warning("Internal connectivity state changed by request originating from outside ConnectivityService");
+    _connectivity = result;
+  }
+  
   Future<void> initialize() async {
     final conn = Connectivity();
     _connectivity = await conn.checkConnectivity();
@@ -23,10 +30,10 @@ class ConnectivityService {
     // See https://github.com/fluttercommunity/plus_plugins/issues/567
     final skipAmount = Platform.isAndroid ? 1 : 0;
     conn.onConnectivityChanged.skip(skipAmount).listen((ConnectivityResult result) {
-        _connectivity = result;
+      _connectivity = result;
 
-        // Notify other services
-        GetIt.I.get<MoxxyReconnectionPolicy>().onConnectivityChanged(result);
+      // Notify other services
+      GetIt.I.get<MoxxyReconnectionPolicy>().onConnectivityChanged(result);
     });
   }
 
