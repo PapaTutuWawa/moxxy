a5e0df02f6e668b5645a76b75bd1c7d03b063ea4
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-07-22 19:22:24 +0200

    ui: Add logging to UIDownloadService

--- lib/ui/service/download.dart
+++ lib/ui/service/download.dart
@@ -1,21 +1,31 @@
+import 'package:logging/logging.dart';
+
 typedef UIDownloadCallback = void Function(double);
 
 /// This class handles download progress notifications from the backend and relays them
 /// to the correct ChatBubble instance so that it can update itself.
 class UIDownloadService {
-  UIDownloadService() : _callbacks = {};
+  UIDownloadService() : _callbacks = {}, _log = Logger('UIDownloadService');
 
+  final Logger _log;
   // Database message id -> callback function
   final Map<int, UIDownloadCallback> _callbacks;
 
   void registerCallback(int id, UIDownloadCallback callback) {
+    _log.finest('Registering callback for $id');
     _callbacks[id] = callback;
   }
 
   void unregisterCallback(int id) {
+    _log.finest('Unregistering callback for $id');
     _callbacks.remove(id);
   }
 
+  void unregisterAll() {
+    _log.finest('Unregistering all callbacks');
+    _callbacks.clear();
+  }
+  
   void onProgress(int id, double progress) {
     if (_callbacks.containsKey(id)) {
       if (progress == 1.0) {
@@ -24,7 +34,7 @@ class UIDownloadService {
         _callbacks[id]!(progress);
       }
     } else {
-      // TODO(Unknown): Log
+      _log.warning('Received progress callback for unregistered id $id');
     }
   }
 }
