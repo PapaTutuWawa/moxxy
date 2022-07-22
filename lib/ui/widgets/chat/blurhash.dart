25221cba1b7790adbb99b47e2cd1184cc5e802b4
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-07-22 20:38:06 +0200

    ui(wip): Migrate to flutter_blurhash

--- lib/ui/widgets/chat/blurhash.dart
+++ lib/ui/widgets/chat/blurhash.dart
@@ -1,7 +1,5 @@
-import 'dart:typed_data';
-
-import 'package:blurhash/blurhash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:logging/logging.dart';
 
 class BlurhashChatWidget extends StatelessWidget {
@@ -25,37 +23,25 @@ class BlurhashChatWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Uint8List?>(
-      future: (() async {
-        try {
-          return await BlurHash.decode(thumbnailData, width, height);
-        } on Exception catch(e) {
-          _log.warning(e.toString());
-        }
-
-        return null;
-      })(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return IntrinsicWidth(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: borderRadius,
-                  child: Image.memory(snapshot.data!),
-                ),
-                ...child != null ? [child!] : []
-              ],
+    return IntrinsicWidth(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: borderRadius,
+            child: SizedBox(
+              width: width.toDouble(),
+              height: height.toDouble(),
+              child: BlurHash(
+                hash: thumbnailData,
+                decodingWidth: width, 
+                decodingHeight: height,
+              ),
             ),
-          );
-        }
-
-        return Padding(
-          padding: const EdgeInsets.all(8),
-          child: child,
-        );
-      },
+          ),
+          ...child != null ? [child!] : []
+        ],
+      ),
     );
   }
 }
