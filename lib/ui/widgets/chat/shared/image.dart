626215153284ba971d9964014daaf782c8bd8ad4
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-07-24 14:48:23 +0200

    ui: Migrate SharedImage to ImageThumbnailWidget

--- lib/ui/widgets/chat/shared/image.dart
+++ lib/ui/widgets/chat/shared/image.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:moxxyv2/ui/service/thumbnail.dart';
 import 'package:moxxyv2/ui/widgets/chat/shared/base.dart';
+import 'package:moxxyv2/ui/widgets/chat/thumbnail.dart';
 import 'package:open_file/open_file.dart';
 
 class SharedImageWidget extends StatelessWidget {
@@ -16,28 +17,9 @@ class SharedImageWidget extends StatelessWidget {
     return SharedMediaContainer(
       ClipRRect(
         borderRadius: BorderRadius.circular(10),
-        child: FutureBuilder<Uint8List>(
-          future: GetIt.I.get<ThumbnailCacheService>().getImageThumbnail(path),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.data != null) {
-                return Image.memory(
-                  snapshot.data!,
-                  fit: BoxFit.cover,
-                );
-              } else {
-                return const Padding(
-                  padding: EdgeInsets.all(32),
-                  child: Icon(
-                    Icons.error_outline,
-                    size: 32,
-                  ),
-                );
-              }
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
+        child: ImageThumbnailWidget(
+          path,
+          (data) => Image.memory(data, fit: BoxFit.cover),
         ),
       ),
       onTap: () => OpenFile.open(path),
