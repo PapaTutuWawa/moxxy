1327e6435c00318ef6d64515bf808654479648f9
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-07-29 11:33:00 +0200

    ui: Guard the thumbnail cache with a semaphore

--- lib/ui/service/thumbnail.dart
+++ lib/ui/service/thumbnail.dart
@@ -5,7 +5,7 @@ import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter_isolate/flutter_isolate.dart';
 import 'package:logging/logging.dart';
 import 'package:moxxyv2/shared/cache.dart';
-import 'package:synchronized/synchronized.dart';
+import 'package:moxxyv2/shared/semaphore.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 
 Future<void> _generateVideoThumbnail(List<dynamic> values) async {
@@ -40,71 +40,70 @@ class ThumbnailCacheService {
   ThumbnailCacheService()
   // TODO(Unknown): Maybe raise this limit
   : _thumbnailCache = LRUCache(200),
-    _cacheLock = Lock(),
+    _cacheSemaphore = Semaphore(8),
     _log = Logger('ThumbnailCacheService');
   final Logger _log;
 
   // Asset path -> decoded data
   final LRUCache<String, Uint8List> _thumbnailCache;
-  final Lock _cacheLock;
+  final Semaphore _cacheSemaphore;
 
   Future<Uint8List> getVideoThumbnail(String path) async {
-    Uint8List? data;
-
     // Turning this into a critical section allows us to prevent multiple calls to the
     // isolate in case we generate thumbnails for the same path multiple times.
-    _log.fine('getVideoThumbnail: Waiting to acquire lock...');
-    await _cacheLock.synchronized(() async {
-        _log.fine('getVideoThumbnail: Done');
-        if (_thumbnailCache.inCache(path)) {
-          _log.finest('Thumbnail data is in cache!');
-          data = _thumbnailCache.getValue(path);
-          _log.finest('getVideoThumbnail: Releasing lock');
-          return;
-        }
+    _log.fine('getVideoThumbnail: Waiting to acquire semaphore...');
+    await _cacheSemaphore.aquire();
+    _log.fine('getVideoThumbnail: Done');
+    if (_thumbnailCache.inCache(path)) {
+      _log.finest('Thumbnail data is in cache!');
+      await _cacheSemaphore.release();
+      _log.finest('getVideoThumbnail: Released semaphore');
 
-        _log.finest('Thumbnail data not in cache generating...');
+      return _thumbnailCache.getValue(path)!;
+    }
 
-        final port = ReceivePort();
-        final isolate = await FlutterIsolate.spawn(_generateVideoThumbnail, [ port.sendPort, path ]);
-        data = await port.first as Uint8List;
-        isolate.kill(priority: Isolate.immediate);
-        
-        _log.finest('Generation done.');
-        _thumbnailCache.cache(path, Uint8List.fromList(data!));
-        _log.finest('getVideoThumbnail: Releasing lock');
-    });
+    _log.finest('Thumbnail data not in cache generating...');
 
-    return data!;
+    final port = ReceivePort();
+    final isolate = await FlutterIsolate.spawn(_generateVideoThumbnail, [ port.sendPort, path ]);
+    final data = await port.first as Uint8List;
+    isolate.kill(priority: Isolate.immediate);
+    
+    _log.finest('Generation done.');
+    _thumbnailCache.cache(path, Uint8List.fromList(data));
+
+    await _cacheSemaphore.release();
+    _log.finest('getVideoThumbnail: Released semaphore');
+
+    return data;
   }
 
   Future<Uint8List> getImageThumbnail(String path) async {
-    Uint8List? data;
-
     // Turning this into a critical section allows us to prevent multiple calls to the
     // isolate in case we generate thumbnails for the same path multiple times.
-    _log.finest('getImageThumbnail: Waiting to aquire lock...');
-    await _cacheLock.synchronized(() async {
-        _log.finest('getImageThumbnail: Done');
-        if (_thumbnailCache.inCache(path)) {
-          _log.finest('Thumbnail data is in cache!');
-          data = _thumbnailCache.getValue(path);
-          _log.finest('getImageThumbnail: Releasing lock');
-          return;
-        }
+    _log.finest('getImageThumbnail: Waiting to aquire semaphore...');
+    await _cacheSemaphore.aquire();
+    _log.finest('getImageThumbnail: Done');
+    if (_thumbnailCache.inCache(path)) {
+      _log.finest('Thumbnail data is in cache!');
+      await _cacheSemaphore.release();
+      _log.finest('getImageThumbnail: Released semaphore');
 
-        _log.finest('Thumbnail data not in cache generating...');
+      return _thumbnailCache.getValue(path)!;
+    }
 
-        final port = ReceivePort();
-        final isolate = await FlutterIsolate.spawn(_generateImageThumbnail, [ port.sendPort, path ]);
-        data = await port.first as Uint8List;
-        isolate.kill(priority: Isolate.immediate);
-        
-        _log.finest('Generation done.');
-        _thumbnailCache.cache(path, Uint8List.fromList(data!));
-        _log.finest('getImageThumbnail: Releasing lock');
-    });
+    _log.finest('Thumbnail data not in cache generating...');
 
-    return data!;
+    final port = ReceivePort();
+    final isolate = await FlutterIsolate.spawn(_generateImageThumbnail, [ port.sendPort, path ]);
+    final data = await port.first as Uint8List;
+    isolate.kill(priority: Isolate.immediate);
+    
+    _log.finest('Generation done.');
+    _thumbnailCache.cache(path, Uint8List.fromList(data));
+    await _cacheSemaphore.release();
+    _log.finest('getImageThumbnail: Released semaphore');
+
+    return data;
   }
 }
