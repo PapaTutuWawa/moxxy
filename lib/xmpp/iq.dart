7768277552893bace87967d252acab6ffa0050ef
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-08-17 19:28:32 +0200

    xmpp: Do not bounce result IQs

--- lib/xmpp/iq.dart
+++ lib/xmpp/iq.dart
@@ -2,7 +2,7 @@ import 'package:moxxyv2/xmpp/connection.dart';
 import 'package:moxxyv2/xmpp/stanza.dart';
 
 bool handleUnhandledStanza(XmppConnection conn, Stanza stanza) {
-  if (stanza.type != 'error') {
+  if (stanza.type != 'error' && stanza.type != 'result') {
     conn.sendStanza(stanza.errorReply('cancel', 'feature-not-implemented'));
   }
 
