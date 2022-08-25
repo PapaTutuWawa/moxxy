03a775ef1c87e9e09755d0f0ebe29b542421420f
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-08-25 21:12:57 +0200

    ui: Make URLs clickable

--- lib/ui/widgets/chat/text.dart
+++ lib/ui/widgets/chat/text.dart
@@ -1,8 +1,10 @@
 import 'package:dart_emoji/dart_emoji.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_parsed_text/flutter_parsed_text.dart';
 import 'package:moxxyv2/shared/models/message.dart';
 import 'package:moxxyv2/ui/constants.dart';
 import 'package:moxxyv2/ui/widgets/chat/bottom.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Used whenever the mime type either doesn't match any specific chat widget or we just
 /// cannot determine the mime type.
@@ -32,12 +34,28 @@ class TextChatWidget extends StatelessWidget {
           ...topWidget != null ? [ topWidget! ] : [],
           Padding(
             padding: topWidget != null ? const EdgeInsets.only(left: 8) : EdgeInsets.zero,
-            child: Text(
-              message.body,
+            child: ParsedText(
+              text: message.body,
               style: TextStyle(
                 color: Colors.white,
                 fontSize: fontsize,
               ),
+              parse: [
+                MatchText(
+                  type: ParsedType.URL,
+                  style: const TextStyle(
+                    // TODO(Unknown): Work on the color
+                    color: Colors.blue,
+                  ),
+                  onTap: (url) async {
+                    // TODO(PapaTutuWawa): Implement privacy redirects
+                    await launchUrl(
+                      Uri.parse(url),
+                      mode: LaunchMode.externalNonBrowserApplication,
+                    );
+                  },
+                )
+              ],
             ),
           ),
           Padding(
