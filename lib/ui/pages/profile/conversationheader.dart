80f4104969478d3d6c4f9f4ac1dd5193f66732cc
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-09-06 13:13:31 +0200

    ui: Comment out the subscription button

--- lib/ui/pages/profile/conversationheader.dart
+++ lib/ui/pages/profile/conversationheader.dart
@@ -6,7 +6,7 @@ import 'package:moxxyv2/ui/constants.dart';
 import 'package:moxxyv2/ui/helpers.dart';
 import 'package:moxxyv2/ui/widgets/avatar.dart';
 import 'package:moxxyv2/ui/widgets/chat/shared/base.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
+//import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class ConversationProfileHeader extends StatelessWidget {
 
@@ -15,6 +15,8 @@ class ConversationProfileHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //final subscribed = conversation.subscription == 'both' || conversation.subscription == 'to';
+    
     return Column(
       children: [
         Hero(
@@ -46,7 +48,7 @@ class ConversationProfileHeader extends StatelessWidget {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16),
+          padding: const EdgeInsets.only(top: 16),
           child: Row(
             //mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -55,55 +57,91 @@ class ConversationProfileHeader extends StatelessWidget {
                 message: conversation.muted ?
                   'Unmute chat' :
                   'Mute chat',
-                child: SharedMediaContainer(
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: ColoredBox(
-                      color: getTileColor(context),
-                      child: Icon(
-                        conversation.muted ?
-                          Icons.do_not_disturb_on :
-                          Icons.do_not_disturb_off,
-                        size: 32,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SharedMediaContainer(
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: ColoredBox(
+                          color: getTileColor(context),
+                          child: Icon(
+                            conversation.muted ?
+                            Icons.do_not_disturb_on :
+                            Icons.do_not_disturb_off,
+                            size: 32,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        GetIt.I.get<ProfileBloc>().add(
+                          MuteStateSetEvent(
+                            conversation.jid,
+                            !conversation.muted,
+                          ),
+                        );
+                      },
+                    ),
+                    Text(
+                      conversation.muted ?
+                        'Unmute' :
+                        'Mute',
+                      style: const TextStyle(
+                        fontSize: fontsizeAppbar,
                       ),
                     ),
-                  ),
-                  onTap: () {
-                    GetIt.I.get<ProfileBloc>().add(
-                      MuteStateSetEvent(
-                        conversation.jid,
-                        !conversation.muted,
-                      ),
-                    );
-                  },
+                  ],
                 ),
               ),
+              // TODO(Unknown): How to integrate this into the UI?
+              /* 
               Tooltip(
-                message: 'Stop sharing online status',
-                child: SharedMediaContainer(
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: ColoredBox(
-                      color: getTileColor(context),
-                      child: Icon(
-                        conversation.subscription == 'both' || conversation.subscription == 'to' ?
-                          PhosphorIcons.link :
-                          PhosphorIcons.linkBreak,
-                        size: 32,
+                message: subscribed ?
+                  'Unsubscribe' :
+                  'Subscribe',
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SharedMediaContainer(
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: ColoredBox(
+                          color: getTileColor(context),
+                          child: Icon(
+                            subscribed ?
+                              PhosphorIcons.link :
+                              PhosphorIcons.linkBreak,
+                            size: 32,
+                          ),
+                        ),
                       ),
+                      onTap: () {
+                        GetIt.I.get<ProfileBloc>().add(
+                          SetSubscriptionStateEvent(
+                            conversation.jid,
+                            !subscribed,
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                  onTap: () {
-                    GetIt.I.get<ProfileBloc>().add(
-                      SetSubscriptionStateEvent(
-                        conversation.jid,
-                        // TODO(PapaTutuWawa): Make cleaner
-                        !(conversation.subscription == 'both' || conversation.subscription == 'to'),
-                      ),
-                    );
-                  },
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          subscribed ?
+                          'Unsubscribe' :
+                          'Subscribe',
+                          style: TextStyle(
+                            fontSize: fontsizeAppbar,
+                          ),
+                        ),
+
+                        Icon(Icons.info),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
+              ),*/
             ],
           ),
         ),
