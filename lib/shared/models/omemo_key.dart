5b64506612d74df4b0a9e41b6d403d755f9a87be
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-09-09 17:29:38 +0200

    ui: Add comment about OmemoKey usage

--- lib/shared/models/omemo_key.dart
+++ lib/shared/models/omemo_key.dart
@@ -3,6 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'omemo_key.freezed.dart';
 part 'omemo_key.g.dart';
 
+/// This model is just for communication between UI and the backend.
 @freezed
 class OmemoKey with _$OmemoKey {
   factory OmemoKey(
