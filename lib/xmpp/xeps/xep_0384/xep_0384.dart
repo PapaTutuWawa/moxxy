b8aedc842e0098ac3c146153f30aa9670d95e5a0
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-09-10 21:01:11 +0200

    xmpp: Move conversion functions into the helpers file

--- lib/xmpp/xeps/xep_0384/xep_0384.dart
+++ lib/xmpp/xeps/xep_0384/xep_0384.dart
@@ -45,66 +45,6 @@ bool shouldEncrypt(XMLNode node) {
   return true;
 }
 
-XMLNode bundleToXML(OmemoBundle bundle) {
-  final prekeys = List<XMLNode>.empty(growable: true);
-  for (final pk in bundle.opksEncoded.entries) {
-    prekeys.add(
-      XMLNode(
-        tag: 'pk', attributes: <String, String>{
-          'id': '${pk.key}',
-        },
-        text: pk.value,
-      ),
-    );
-  }
-
-  return XMLNode.xmlns(
-    tag: 'bundle',
-    xmlns: omemoXmlns,
-    children: [
-      XMLNode(
-        tag: 'spk',
-        attributes: <String, String>{
-          'id': '${bundle.spkId}',
-        },
-        text: bundle.spkEncoded,
-      ),
-      XMLNode(
-        tag: 'spks',
-        text: bundle.spkSignatureEncoded,
-      ),
-      XMLNode(
-        tag: 'ik',
-        text: bundle.ikEncoded,
-      ),
-      XMLNode(
-        tag: 'prekeys',
-        children: prekeys,
-      ),
-    ],
-  );
-}
-
-OmemoBundle bundleFromXML(JID jid, int id, XMLNode bundle) {
-  assert(bundle.attributes['xmlns'] == omemoXmlns, 'Invalid xmlns');
-
-  final spk = bundle.firstTag('spk')!;
-  final prekeys = <int, String>{};
-  for (final pk in bundle.firstTag('prekeys')!.findTags('pk')) {
-    prekeys[int.parse(pk.attributes['id']! as String)] = pk.innerText();
-  }
-
-  return OmemoBundle(
-    jid.toBare().toString(),
-    id,
-    spk.innerText(),
-    int.parse(spk.attributes['id']! as String),
-    bundle.firstTag('spks')!.innerText(),
-    bundle.firstTag('ik')!.innerText(),
-    prekeys,
-  );
-}
-
 class OmemoManager extends XmppManagerBase {
 
   OmemoManager(this.omemoState) : super() {
