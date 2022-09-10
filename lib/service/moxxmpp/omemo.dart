5586fcff7a493103da3df40da4107e7885ca8a5d
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-09-10 20:02:15 +0200

    refactor: Move the OMEMO implementation

--- lib/service/moxxmpp/omemo.dart
+++ lib/service/moxxmpp/omemo.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:moxxyv2/service/database/database.dart';
 import 'package:moxxyv2/service/omemo.dart';
-import 'package:moxxyv2/xmpp/xeps/xep_0384.dart';
+import 'package:moxxyv2/xmpp/xeps/xep_0384/xep_0384.dart';
 import 'package:omemo_dart/omemo_dart.dart';
 
 class MoxxyOmemoManager extends OmemoManager {
