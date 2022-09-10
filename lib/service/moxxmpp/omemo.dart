7f366d3f3ce1cc4d81c21b374b432b93df7dd60e
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-09-10 15:09:39 +0200

    service: Publish OMEMO bundle after connecting

--- lib/service/moxxmpp/omemo.dart
+++ lib/service/moxxmpp/omemo.dart
@@ -1,3 +1,6 @@
+import 'package:get_it/get_it.dart';
+import 'package:moxxyv2/service/database/database.dart';
+import 'package:moxxyv2/service/omemo.dart';
 import 'package:moxxyv2/xmpp/xeps/xep_0384.dart';
 import 'package:omemo_dart/omemo_dart.dart';
 
@@ -6,4 +9,10 @@ class MoxxyOmemoManager extends OmemoManager {
   MoxxyOmemoManager(OmemoSessionManager omemoState) : super(omemoState);
 
   // TODO(PapaTutuWawa): Override the storage related methods
+  @override
+  Future<void> commitRatchet(OmemoDoubleRatchet ratchet, String jid, int deviceId) async {
+    await GetIt.I.get<DatabaseService>().saveRatchet(
+      OmemoDoubleRatchetWrapper(ratchet, deviceId, jid),
+    );
+  }
 }
