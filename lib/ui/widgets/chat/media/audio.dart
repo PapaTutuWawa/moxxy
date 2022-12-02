8f5470076bb67c08b8f96774586a9f8946553dbc
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-12-02 16:55:02 +0100

    feat(ui): Implement UI for audio messages during up/download

--- lib/ui/widgets/chat/media/audio.dart
+++ lib/ui/widgets/chat/media/audio.dart
@@ -28,6 +28,85 @@ enum _AudioPlaybackState {
   stopped
 }
 
+class _AudioWidget extends StatelessWidget {
+  const _AudioWidget(
+    this.maxWidth,
+    this.isDownloading,
+    this.onTap,
+    this.icon,
+    this.onChanged,
+    this.duration,
+    this.position,
+    this.messageId,
+  );
+  final double maxWidth;
+  final bool isDownloading;
+  final void Function() onTap;
+  final void Function(double) onChanged;
+  final double? duration;
+  final double? position;
+  final Widget? icon;
+  final int messageId;
+
+  Widget _getLeftWidget() {
+    if (isDownloading) {
+      return SizedBox(
+        width: 48,
+        height: 48,
+        child: ProgressWidget(id: messageId),
+      );
+    }
+
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 8,
+          right: 4,
+        ),
+        child: icon,
+      ),
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: maxWidth,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _getLeftWidget(),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Slider(
+                onChanged: onChanged,
+                value: (position != null && duration != null) ?
+                  (position! / duration!).clamp(0, 1) :
+                  0,
+              ),
+
+              SizedBox(
+                width: maxWidth - 80,
+                child: Row(
+                  children: [
+                    Text(doubleToTimestamp(position ?? 0)),
+                    const Spacer(),
+                    Text(doubleToTimestamp(duration ?? 0)),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 20),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class AudioChatWidget extends StatefulWidget {
   const AudioChatWidget(
     this.message,
@@ -94,100 +173,79 @@ class AudioChatState extends State<AudioChatWidget> {
   }
   
   Widget _buildUploading() {
-    // TODO(PapaTutuWawa): Fix
-    return FileChatWidget(
-      widget.message,
+    return MediaBaseChatWidget(
+      _AudioWidget(
+        widget.maxWidth,
+        true,
+        () {},
+        null,
+        (_) {},
+        null,
+        null,
+        widget.message.id,
+      ),
+      MessageBubbleBottom(widget.message, widget.sent),
       widget.radius,
-      widget.sent,
-      extra: ProgressWidget(id: widget.message.id),
+      gradient: false,
     );
   }
 
   Widget _buildDownloading() {
-    // TODO(PapaTutuWawa): Fix
-    return FileChatBaseWidget(
-      widget.message,
-      Icons.image,
-      widget.message.isFileUploadNotification ?
-        (widget.message.filename ?? '') :
-        filenameFromUrl(widget.message.srcUrl!),
+    return MediaBaseChatWidget(
+      _AudioWidget(
+        widget.maxWidth,
+        true,
+        () {},
+        null,
+        (_) {},
+        null,
+        null,
+        widget.message.id,
+      ),
+      MessageBubbleBottom(widget.message, widget.sent),
       widget.radius,
-      widget.sent,
-      extra: ProgressWidget(id: widget.message.id),
+      gradient: false,
     );
   }
   
   /// The audio file exists locally
   Widget _buildAudio() {
     return MediaBaseChatWidget(
-      SizedBox(
-        width: widget.maxWidth,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            InkWell(
-              onTap: () {
-                if (_playState != _AudioPlaybackState.playing) {
-                  if (_playState == _AudioPlaybackState.paused) {
-                    _audioFile?.resume();
-                  } else if (_playState == _AudioPlaybackState.stopped) {
-                    _audioFile?.play();
-                  }
+      _AudioWidget(
+        widget.maxWidth,
+        false,
+        () {
+          if (_playState != _AudioPlaybackState.playing) {
+            if (_playState == _AudioPlaybackState.paused) {
+              _audioFile?.resume();
+            } else if (_playState == _AudioPlaybackState.stopped) {
+              _audioFile?.play();
+            }
 
-                  setState(() {
-                    _playState = _AudioPlaybackState.playing;
-                  });
-                } else {
-                  _audioFile?.pause();
-                  setState(() {
-                    _playState = _AudioPlaybackState.paused;
-                  });
-                }
+            setState(() {
+              _playState = _AudioPlaybackState.playing;
+            });
+          } else {
+            _audioFile?.pause();
+            setState(() {
+              _playState = _AudioPlaybackState.paused;
+            });
+          }
+        },
+        _playState == _AudioPlaybackState.playing ?
+          const Icon(Icons.pause) :
+          const Icon(Icons.play_arrow),
+        (p) {
+          if (_duration == null || _audioFile == null) return;
 
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 8,
-                  right: 4,
-                ),
-                child: _playState == _AudioPlaybackState.playing ?
-                  const Icon(Icons.pause) :
-                  const Icon(Icons.play_arrow),
-              ),
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Slider(
-                  onChanged: (p) {
-                    if (_duration == null || _audioFile == null) return;
-
-                    setState(() {
-                      _position = p * _duration!;
-                      _audioFile!.seek(_position!);
-                    });
-                  },
-                  value: (_position != null && _duration != null) ?
-                    (_position! / _duration!).clamp(0, 1) :
-                    0,
-                ),
-
-                SizedBox(
-                  width: widget.maxWidth - 80,
-                  child: Row(
-                    children: [
-                      Text(doubleToTimestamp(_position ?? 0)),
-                      const Spacer(),
-                      Text(doubleToTimestamp(_duration ?? 0)),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-              ],
-            ),
-          ],
-        ),
+          setState(() {
+              _position = p * _duration!;
+              _audioFile!.seek(_position!);
+          });
+        },
+        _duration,
+        _position,
+        widget.message.id,
       ),
       MessageBubbleBottom(widget.message, widget.sent),
       widget.radius,
@@ -196,6 +254,7 @@ class AudioChatState extends State<AudioChatWidget> {
   }
 
   Widget _buildDownloadable() {
+    // TODO(Unknown): Implement
     return FileChatBaseWidget(
       widget.message,
       Icons.image,
@@ -222,7 +281,7 @@ class AudioChatState extends State<AudioChatWidget> {
 
     // TODO(PapaTutuWawa): Maybe use an async builder
     if (widget.message.mediaUrl != null && File(widget.message.mediaUrl!).existsSync()) return _buildAudio();
-
+    
     return _buildDownloadable();
   }
 }
