f4928452354e22a1408b32860796a50373db796d
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-12-17 13:45:08 +0100

    feat(ui): Somewhat handle not locally available stickers

--- lib/ui/widgets/chat/media/sticker.dart
+++ lib/ui/widgets/chat/media/sticker.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:moxxyv2/shared/models/message.dart';
+import 'package:moxxyv2/shared/models/sticker.dart';
+import 'package:moxxyv2/ui/bloc/preferences_bloc.dart';
 import 'package:moxxyv2/ui/bloc/stickers_bloc.dart';
 import 'package:moxxyv2/ui/constants.dart';
 import 'package:moxxyv2/ui/widgets/chat/bottom.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class StickerChatWidget extends StatelessWidget {
   const StickerChatWidget(
@@ -20,17 +23,59 @@ class StickerChatWidget extends StatelessWidget {
   final double maxWidth;
   final BorderRadius radius;
   final bool sent;
-
+  
+  Widget _buildNotAvailable() {
+    return Align(
+      alignment: sent ?
+        Alignment.centerRight :
+        Alignment.centerLeft,
+        child: InkWell(
+          onTap: () {
+            // TODO(PapaTutuWawa): If not locally available, show the download dialog
+          },
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: sent ?
+                bubbleColorSent :
+                bubbleColorReceived,
+              borderRadius: BorderRadius.all(radiusLarge),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 8),
+                    child: Icon(
+                      PhosphorIcons.stickerBold,
+                    ),
+                  ),
+                  Text(
+                    message.body,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
-    // TODO(PapaTutuWawa): Handle stickers we don't have
-    final stickerKey = StickerKey(message.stickerPackId!, message.stickerId!);
-    final sticker = GetIt.I.get<StickersBloc>().state.stickerMap[stickerKey]!;
+    Sticker? sticker;
+    if (message.stickerPackId != null && message.stickerId != null) {
+      final stickerKey = StickerKey(message.stickerPackId!, message.stickerId!);
+      sticker = GetIt.I.get<StickersBloc>().state.stickerMap[stickerKey];
+    }
 
     return IntrinsicHeight(
       child: Column(
         children: [
-          Image.file(File(sticker.path)),
+          sticker != null && GetIt.I.get<PreferencesBloc>().state.enableStickers ?
+            Image.file(File(sticker.path)) :
+            _buildNotAvailable(),
 
           Align(
             alignment: sent ?
