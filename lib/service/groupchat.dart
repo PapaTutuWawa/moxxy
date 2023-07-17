7c840334e11d4f5d9ea44a9410957f6ec29ec454
Ikjot Singh Dhody <ikjotsd@gmail.com>
2023-07-17 19:47:28 +0530

    feat(all): Add docs to groupchat service methods.
    Signed-off-by: Ikjot Singh Dhody <ikjotsd@gmail.com>

--- lib/service/groupchat.dart
+++ lib/service/groupchat.dart
@@ -6,6 +6,10 @@ import 'package:moxxyv2/shared/error_types.dart';
 import 'package:moxxyv2/shared/models/groupchat.dart';
 
 class GroupchatService {
+  /// Retrieves the information about a group chat room specified by the given
+  /// JID.
+  /// Returns a [Future] that resolves to a [RoomInformation] object containing
+  /// details about the room.
   Future<RoomInformation> getRoomInformation(JID roomJID) async {
     final conn = GetIt.I.get<XmppConnection>();
     final mm = conn.getManagerById<MUCManager>(mucManager)!;
@@ -16,6 +20,11 @@ class GroupchatService {
     throw Exception(result.get<MUCError>());
   }
 
+  /// Joins a group chat room specified by the given MUC JID and a nickname.
+  /// Returns a [Future] that resolves to a [GroupchatDetails] object
+  /// representing the details of the joined room.
+  /// Throws an exception of type [GroupchatErrorType.roomPasswordProtected]
+  /// if the room requires a password for entry.
   Future<GroupchatDetails> joinRoom(JID muc, String nick) async {
     final conn = GetIt.I.get<XmppConnection>();
     final mm = conn.getManagerById<MUCManager>(mucManager)!;
@@ -37,6 +46,10 @@ class GroupchatService {
     }
   }
 
+  /// Creates and adds group chat details to the database based on the provided
+  /// JID, nickname, and title.
+  /// Returns a [Future] that resolves to a [GroupchatDetails] object
+  /// representing the added group chat details.
   Future<GroupchatDetails> addGroupchatDetailsFromData(
     String jid,
     String nick,
@@ -51,6 +64,10 @@ class GroupchatService {
     return groupchatDetails;
   }
 
+  /// Retrieves group chat details from the database based on the provided JID.
+  ///
+  /// Returns a [Future] that resolves to a [GroupchatDetails] object if found,
+  /// or `null` if no matching details are found.
   Future<GroupchatDetails?> getGroupchatDetailsByJid(String jid) async {
     final db = GetIt.I.get<DatabaseService>().database;
     final groupchatDetailsRaw = await db.query(
